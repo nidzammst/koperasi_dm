0fedb970b5d71ebd3335a8af3b45f5e320c89216
nidzammst <akamhacks@gmail.com>
2024-02-16 09:57:46 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const { errorHandler, notFound } = require('./middlewares/errorHandler');
 const dbConnect = require('./config/dbConnect');
@@ -18,7 +19,12 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+var corsOptions = {
+  origin: 'https://3000-wandering-sound-82687444.in-ws2.runcode.io/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+app.use(cors(corsOptions));
 app.use('/api/account', accountRouter);
 app.use('/api/product', productRouter);
 app.use('/api/payment', paymentRouter);
